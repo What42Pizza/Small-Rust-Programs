4eb6642a12fff54e782ae0b95ff212f70fb6dc49
What42Pizza <47263768+What42Pizza@users.noreply.github.com>
2024-04-03 09:36:17 -0500

    remove confirmation

--- clear-all-targets/src/main.rs
+++ clear-all-targets/src/main.rs
@@ -14,7 +14,6 @@ fn main() -> Result<()> {
 	let top_folder = prompt!("Folder to search: ");
 	let ignore_errors = prompt!("Ignore errors? "; [true] YesNoInput);
 	
-	let mut project_paths = vec!();
 	let mut i = 0;
 	for entry in WalkDir::new(top_folder) {
 		
@@ -29,7 +28,7 @@ fn main() -> Result<()> {
 		if !path.is_dir() {continue;}
 		
 		if i % 1000 == 0 {
-			println!("{i}");
+			println!("Folders searched: {i}");
 		}
 		i += 1;
 		
@@ -38,52 +37,14 @@ fn main() -> Result<()> {
 		if !path.join("target").exists() {continue;}
 		
 		println!("Found rust project: {path:?}");
-		project_paths.push((true, path));
-		
-	}
-	
-	loop {
-		println!();
-		println!();
-		println!();
-		println!("All found projects:");
-		for (i, (is_enabled, path)) in project_paths.iter().enumerate() {
-			if *is_enabled {
-				println!("{i}: {path:?}");
-			} else {
-				println!("{i}: {path:?} (disabled)");
-			}
-		}
-		let option = prompt!("Enter \"cancel\", \"clean\", or index of path to toggle: ");
-		match &*option {
-			"cancel" => return Ok(()),
-			"clean" => break,
-			_ => {
-				let StdResult::Ok(index) = option.parse::<usize>() else {
-					println!("Could not parse input.");
-					continue;
-				};
-				project_paths[index].0 = !project_paths[index].0;
-			}
-		}
-	}
-	
-	println!();
-	println!();
-	println!();
-	let ignore_errors = prompt!("About to start cleaning, ignore errors? "; [true] YesNoInput);
-	
-	for (is_enabled, path) in project_paths {
-		if !is_enabled {continue;}
-		println!("Cleaning {path:?}...");
 		let result = Command::new("cargo")
 			.args(["clean"])
 			.current_dir(path)
 			.output();
-		println!("{result:?}");
 		if let Err(err) = result {
 			if !ignore_errors {return Err(err.into());}
 		}
+		
 	}
 	
 	Ok(())
